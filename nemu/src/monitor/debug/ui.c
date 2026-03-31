856b2b12920f61083a0911d96415faba135b18d3
tracer-ics2017 <tracer@njuics.org>
2026-03-31 20:24:03 +0800

    >  compile 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-14-generic #14~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jan 15 15:52:10 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:24:03 up  1:37,  1 user,  load average: 0.13, 0.05, 0.01 b3a04aca40a4ab99bf2a23bc7875267be5c1f2d4

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,15 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args) {
+    int step = 1;  
+    if (args != NULL) {
+        sscanf(args, "%d", &step);
+    }
+    cpu_exec(step);  
+    return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,7 +55,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si",   "Single step execution, si N = execute N steps", cmd_si },
   /* TODO: Add more commands */
 
 };
