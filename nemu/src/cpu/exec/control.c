ae167925c3bd133e4c55577af58ebdd6b4d25f5e
tracer-ics2017 <tracer@njuics.org>
2026-04-21 16:21:02 +0800

    >  compile 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-20-generic #20~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 19 01:28:37 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:21:02 up 35 min,  1 user,  load average: 0.17, 0.09, 0.12 b123c4e572fa9d2ffe3ce0a3f1934b9a570556f6

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,12 +25,10 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-   rtl_push(&cpu.eip);
-  decoding.jmp_eip = id_dest->val;
+  rtl_push(&cpu.eip);
   decoding.is_jmp = 1;
 
   print_asm("call %x", decoding.jmp_eip);
-
 }
 
 make_EHelper(ret) {
