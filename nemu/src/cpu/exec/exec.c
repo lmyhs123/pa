a2b9da582ff139bb4b44d62bb36c2603582a7cae
tracer-ics2017 <tracer@njuics.org>
2026-04-21 19:20:04 +0800

    >  compile 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-20-generic #20~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 19 01:28:37 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:20:04 up  1:04,  1 user,  load average: 1.00, 0.93, 0.58 2472c4ac8d1f2b998bf0fc411a0a323172cf0446

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -36,6 +36,7 @@ static make_EHelper(2byte_esc);
     /* 0x04 */	item4, item5, item6, item7  \
   }; \
 static make_EHelper(name) { \
+  printf("DEBUG gp1: idx=%d opcode=0x%02x\n", decoding.ext_opcode, decoding.opcode); \
   idex(eip, &concat(opcode_table_, name)[decoding.ext_opcode]); \
 }
 
