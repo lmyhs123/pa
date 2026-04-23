0897434a9bd907dcb6b607b575af26031aab46ab
tracer-ics2017 <tracer@njuics.org>
2026-04-23 20:30:35 +0800

    >  compile 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:30:35 up  6:06,  3 users,  load average: 0.13, 0.06, 0.01 f96a1fa43cb1e0aba8f262c885a7bf6dcedfaa5

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -1,9 +1,7 @@
 #include "cpu/exec.h"
 
 make_EHelper(mov);
-
 make_EHelper(operand_size);
-
 make_EHelper(inv);
 make_EHelper(nemu_trap);
 make_EHelper(nop);
@@ -21,3 +19,4 @@ make_EHelper(lea);
 make_EHelper(setcc);
 make_EHelper(movzx);
 make_EHelper(test);
+make_EHelper(jcc);
