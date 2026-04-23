81c17f24b1d69ff165352dec8e0c3eb8da52ea92
tracer-ics2017 <tracer@njuics.org>
2026-04-23 20:39:54 +0800

    >  compile 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:39:54 up  6:16,  3 users,  load average: 0.36, 0.20, 0.06 96157428516a889fa240e786434bae5fa1cc0458

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -46,7 +46,7 @@ static make_EHelper(name) { \
 /* 0x80, 0x81, 0x83 */
 make_group(gp1,
     EX(add), EMPTY, EMPTY, IDEX(SI2E, sub),
-    EX(and), IDEXW(I2E, sub, 1), EMPTY, EMPTY)
+    EX(and), IDEXW(I2E, sub, 1), EMPTY, EX(cmp))
 
 /* TODO: Add more instructions!!! */
 
