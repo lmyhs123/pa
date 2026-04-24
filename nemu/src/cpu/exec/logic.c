b4d4f1281ceb98394d68d3311df274a32e66e558
tracer-ics2017 <tracer@njuics.org>
2026-04-24 14:31:35 +0800

    >  compile 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:31:35 up  7:27,  3 users,  load average: 0.00, 0.04, 0.07 d3030095225ac37d6bead2b0ccb73582106be4e5

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -39,16 +39,16 @@ make_EHelper(or) {
   print_asm_template2(or);
 }
 
-make_EHelper(sar) {
-  TODO();
-  // unnecessary to update CF and OF in NEMU
-
-  print_asm_template2(sar);
-}
-
 make_EHelper(shl) {
-  TODO();
-  // unnecessary to update CF and OF in NEMU
+  // CF = old MSB before shift
+  rtl_msb(&t0, &id_dest->val, id_dest->width);
+  rtl_shl(&t1, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t1);
+
+  rtl_update_ZFSF(&t1, id_dest->width);
+  rtl_set_CF(&t0);
+  // OF undefined for shift count > 1; set 0 for simplicity
+  rtl_set_OF(&tzero);
 
   print_asm_template2(shl);
 }
@@ -65,6 +65,20 @@ make_EHelper(shr) {
   rtl_set_OF(&tzero);
 
   print_asm_template2(shr);
+}
+
+make_EHelper(sar) {
+  // CF = old MSB before shift
+  rtl_msb(&t0, &id_dest->val, id_dest->width);
+  rtl_sar(&t1, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t1);
+
+  rtl_update_ZFSF(&t1, id_dest->width);
+  rtl_set_CF(&t0);
+  // OF undefined for shift count > 1; set 0 for simplicity
+  rtl_set_OF(&tzero);
+
+  print_asm_template2(sar);
 }
 
 make_EHelper(setcc) {
