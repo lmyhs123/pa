4752afa2184f598486afc7866e7b0cde3678c6b4
tracer-ics2017 <tracer@njuics.org>
2026-04-25 21:41:00 +0800

    >  compile 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-22-generic #22~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Mar 26 15:25:54 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:41:00 up 1 day,  3:36,  3 users,  load average: 0.32, 0.13, 0.10 450d107cf8cdc236268fa927d7fab050188a2a

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -4,40 +4,77 @@
 
 void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   switch (subcode) {
-    case 0x4:
+    case 0x4:  // e / z
       rtl_get_ZF(dest);
       break;
-    case 0x5:
+
+    case 0x5:  // ne / nz
       rtl_get_ZF(dest);
       rtl_xori(dest, dest, 1);
       break;
-      case 0x7: {  // a / nbe
-  rtl_get_CF(dest);
-  rtl_xori(dest, dest, 1);
-  rtl_get_ZF(&t0);
-  rtl_xori(&t0, &t0, 1);
-  rtl_and(dest, dest, &t0);
-  break;
-}
 
-case 0x6: {  // be / na
-  rtl_get_CF(dest);
-  rtl_get_ZF(&t0);
-  rtl_or(dest, dest, &t0);
-  break;
-}
+    case 0x6: {  // be / na
+      rtl_get_CF(dest);
+      rtl_get_ZF(&t0);
+      rtl_or(dest, dest, &t0);
+      break;
+    }
 
-case 0x8:  // s
-  rtl_get_SF(dest);
-  break;
+    case 0x7: {  // a / nbe
+      rtl_get_CF(dest);
+      rtl_xori(dest, dest, 1);
+      rtl_get_ZF(&t0);
+      rtl_xori(&t0, &t0, 1);
+      rtl_and(dest, dest, &t0);
+      break;
+    }
 
+    case 0x8:  // s
+      rtl_get_SF(dest);
+      break;
 
-  case 0x9:  // ns
-  rtl_get_SF(dest);
-  rtl_xori(dest, dest, 1);
-  break;
+    case 0x9:  // ns
+      rtl_get_SF(dest);
+      rtl_xori(dest, dest, 1);
+      break;
+
+    case 0xc: {  // l / nge
+      rtl_get_SF(dest);
+      rtl_get_OF(&t0);
+      rtl_xor(dest, dest, &t0);
+      break;
+    }
+
+    case 0xd: {  // ge / nl
+      rtl_get_SF(dest);
+      rtl_get_OF(&t0);
+      rtl_xor(dest, dest, &t0);
+      rtl_xori(dest, dest, 1);
+      break;
+    }
+
+    case 0xe: {  // le / ng
+      rtl_get_SF(dest);
+      rtl_get_OF(&t0);
+      rtl_xor(dest, dest, &t0);   // SF != OF
+      rtl_get_ZF(&t1);
+      rtl_or(dest, dest, &t1);    // ZF || (SF != OF)
+      break;
+    }
+
+    case 0xf: {  // g / nle
+      rtl_get_SF(dest);
+      rtl_get_OF(&t0);
+      rtl_xor(dest, dest, &t0);   // SF != OF
+      rtl_xori(dest, dest, 1);    // SF == OF
+      rtl_get_ZF(&t1);
+      rtl_xori(&t1, &t1, 1);      // !ZF
+      rtl_and(dest, dest, &t1);   // !ZF && (SF == OF)
+      break;
+    }
 
     default:
-   
+      TODO();
   }
 }
+
