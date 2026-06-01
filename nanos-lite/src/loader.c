e008ca771d7e6d006254b45d4228f689ee10b984
tracer-ics2017 <tracer@njuics.org>
2026-06-01 21:26:03 +0800

    >  run 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 11 10:30:58 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:26:03 up  2:03,  2 users,  load average: 0.00, 0.02, 0.00 ad25376c5ec74e454c53c563ea2bf290533adb28

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -4,8 +4,13 @@
 
 void ramdisk_read(void *buf, off_t offset, size_t len);
 size_t get_ramdisk_size();
+bool gdb_memcpy_to_qemu(uint32_t, void *, int);
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
+  size_t size = get_ramdisk_size();
+  ramdisk_read(DEFAULT_ENTRY, 0, size);
+#ifdef DIFF_TEST
+  gdb_memcpy_to_qemu((uint32_t)DEFAULT_ENTRY, DEFAULT_ENTRY, size);
+#endif
   return (uintptr_t)DEFAULT_ENTRY;
 }
