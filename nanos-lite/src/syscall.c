b39e99a7cf8522ca992718254b30dee5470f1547
tracer-ics2017 <tracer@njuics.org>
2026-06-01 22:05:59 +0800

    >  run 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 11 10:30:58 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:05:59 up  2:43,  2 users,  load average: 0.00, 0.01, 0.00 1a5adf86e2dc4ed3feec66ca67c6bf03787ce0c6

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -4,10 +4,14 @@
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
+  a[1] = SYSCALL_ARG2(r);
 
   switch (a[0]) {
     case SYS_none:
       r->eax = 1;
+      return r;
+    case SYS_exit:
+      _halt(a[1]);
       return r;
     default:
       panic("Unhandled syscall ID = %d", a[0]);
