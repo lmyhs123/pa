ef7e54727d95d965686a26ab67fe493c264579f7
tracer-ics2017 <tracer@njuics.org>
2026-06-02 23:45:38 +0800

    >  run 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 11 10:30:58 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:45:38 up  6:05,  2 users,  load average: 0.13, 0.37, 0.63 21615f14c6af973f7f30e56df673b1c57c6ba5ac

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -35,8 +35,13 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
 void _draw_sync() {
 }
 
-#define KBD_PORT 0x60
+#define KBD_DATA_PORT 0x60
+#define KBD_STATUS_PORT 0x64
+#define KBD_STATUS_HASKEY 0x1
 
 int _read_key() {
-  return inl(KBD_PORT);
+  if ((inl(KBD_STATUS_PORT) & KBD_STATUS_HASKEY) == 0) {
+    return _KEY_NONE;
+  }
+  return inl(KBD_DATA_PORT);
 }
