7a6b5d1529f8bcef749584606d970f56aa4404da
tracer-ics2017 <tracer@njuics.org>
2026-06-02 20:33:40 +0800

    >  run 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 11 10:30:58 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:33:40 up  2:53,  3 users,  load average: 0.00, 0.02, 0.01 94e8b34a943fa15f6fc1ad848464e853ba5e1a11

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -4,6 +4,7 @@ typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -20,6 +21,93 @@ static Finfo file_table[] __attribute__((used)) = {
 };
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
+
+void ramdisk_read(void *buf, off_t offset, size_t len);
+void ramdisk_write(const void *buf, off_t offset, size_t len);
+size_t events_read(void *buf, size_t len);
+void dispinfo_read(void *buf, off_t offset, size_t len);
+void fb_write(const void *buf, off_t offset, size_t len);
+
+int fs_open(const char *pathname, int flags, int mode) {
+  for (int i = 0; i < NR_FILES; i ++) {
+    if (strcmp(pathname, file_table[i].name) == 0) {
+      file_table[i].open_offset = 0;
+      return i;
+    }
+  }
+
+  panic("Can not open file: %s", pathname);
+  return -1;
+}
+
+size_t fs_read(int fd, void *buf, size_t len) {
+  assert(fd >= 0 && fd < NR_FILES);
+
+  if (fd == FD_STDIN) return 0;
+  if (fd == FD_EVENTS) return events_read(buf, len);
+  if (fd == FD_DISPINFO) {
+    if (file_table[fd].open_offset + len > file_table[fd].size) {
+      len = file_table[fd].size - file_table[fd].open_offset;
+    }
+    dispinfo_read(buf, file_table[fd].open_offset, len);
+    file_table[fd].open_offset += len;
+    return len;
+  }
+
+  if (file_table[fd].open_offset + len > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
+
+  ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += len;
+  return len;
+}
+
+size_t fs_write(int fd, const void *buf, size_t len) {
+  assert(fd >= 0 && fd < NR_FILES);
+
+  if (fd == FD_STDOUT || fd == FD_STDERR) {
+    const char *s = buf;
+    for (size_t i = 0; i < len; i ++) {
+      _putc(s[i]);
+    }
+    return len;
+  }
+
+  if (fd == FD_FB) {
+    fb_write(buf, file_table[fd].open_offset, len);
+    file_table[fd].open_offset += len;
+    return len;
+  }
+
+  if (file_table[fd].open_offset + len > file_table[fd].size) {
+    len = file_table[fd].size - file_table[fd].open_offset;
+  }
+
+  ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+  file_table[fd].open_offset += len;
+  return len;
+}
+
+off_t fs_lseek(int fd, off_t offset, int whence) {
+  assert(fd >= 0 && fd < NR_FILES);
+
+  switch (whence) {
+    case SEEK_SET: file_table[fd].open_offset = offset; break;
+    case SEEK_CUR: file_table[fd].open_offset += offset; break;
+    case SEEK_END: file_table[fd].open_offset = file_table[fd].size + offset; break;
+    default: panic("Invalid whence = %d", whence);
+  }
+
+  assert(file_table[fd].open_offset >= 0);
+  assert(file_table[fd].open_offset <= file_table[fd].size);
+  return file_table[fd].open_offset;
+}
+
+int fs_close(int fd) {
+  assert(fd >= 0 && fd < NR_FILES);
+  return 0;
+}
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,14 +1,37 @@
 #include "common.h"
+#include "fs.h"
 #include "syscall.h"
+
+int mm_brk(uint32_t new_brk);
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
   a[1] = SYSCALL_ARG2(r);
+  a[2] = SYSCALL_ARG3(r);
+  a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
     case SYS_none:
       r->eax = 1;
+      return r;
+    case SYS_open:
+      r->eax = fs_open((const char *)a[1], a[2], a[3]);
+      return r;
+    case SYS_read:
+      r->eax = fs_read(a[1], (void *)a[2], a[3]);
+      return r;
+    case SYS_write:
+      r->eax = fs_write(a[1], (const void *)a[2], a[3]);
+      return r;
+    case SYS_close:
+      r->eax = fs_close(a[1]);
+      return r;
+    case SYS_lseek:
+      r->eax = fs_lseek(a[1], a[2], a[3]);
+      return r;
+    case SYS_brk:
+      r->eax = mm_brk(a[1]);
       return r;
     case SYS_exit:
       _halt(a[1]);
