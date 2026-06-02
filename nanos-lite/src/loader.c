ea66e14f5b83d42afbd96f955770c1f5027eda1e
tracer-ics2017 <tracer@njuics.org>
2026-06-02 20:45:03 +0800

    >  run 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 11 10:30:58 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:45:03 up  3:04,  3 users,  load average: 0.52, 0.22, 0.12 f12d7603a2d9b50b900856c947424b0b0ae2fc1

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,16 +1,36 @@
 #include "common.h"
+#include "fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
-
-void ramdisk_read(void *buf, off_t offset, size_t len);
-size_t get_ramdisk_size();
-bool gdb_memcpy_to_qemu(uint32_t, void *, int);
+#define INIT_FILE "/etc/init"
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  size_t size = get_ramdisk_size();
-  ramdisk_read(DEFAULT_ENTRY, 0, size);
-#ifdef DIFF_TEST
-  gdb_memcpy_to_qemu((uint32_t)DEFAULT_ENTRY, DEFAULT_ENTRY, size);
-#endif
+  char init[128];
+
+  if (filename == NULL) {
+    int fd = fs_open(INIT_FILE, 0, 0);
+    size_t len = fs_read(fd, init, sizeof(init) - 1);
+    fs_close(fd);
+
+    init[len] = '\0';
+    filename = init;
+    if (init[0] == '#' && init[1] == '!') {
+      filename = init + 2;
+    }
+
+    for (char *p = (char *)filename; *p != '\0'; p ++) {
+      if (*p == '\n' || *p == '\r') {
+        *p = '\0';
+        break;
+      }
+    }
+  }
+
+  int fd = fs_open(filename, 0, 0);
+  size_t size = fs_lseek(fd, 0, SEEK_END);
+  fs_lseek(fd, 0, SEEK_SET);
+  fs_read(fd, DEFAULT_ENTRY, size);
+  fs_close(fd);
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
