b4ef6b8a6483bdf9fe949a598c703172e8bd722a
tracer-ics2017 <tracer@njuics.org>
2026-06-03 12:19:56 +0800

    >  run 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 11 10:30:58 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:19:56 up  6:53,  3 users,  load average: 0.39, 0.58, 0.58 8b328f0760c103b3897044bfd2bfdfb486f2124c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -10,13 +10,16 @@ static const char *keyname[256] __attribute__((used)) = {
 
 size_t events_read(void *buf, size_t len) {
   int key = _read_key();
+  int n;
+
   if (key == _KEY_NONE) {
-    return 0;
+    n = snprintf(buf, len, "t %u\n", _uptime());
+  } else {
+    const char *type = (key & 0x8000) ? "kd" : "ku";
+    int keycode = key & ~0x8000;
+    n = snprintf(buf, len, "%s %s\n", type, keyname[keycode]);
   }
 
-  const char *type = (key & 0x8000) ? "kd" : "ku";
-  int keycode = key & ~0x8000;
-  int n = snprintf(buf, len, "%s %s\n", type, keyname[keycode]);
   return n < len ? n : len;
 }
 
@@ -27,9 +30,24 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-  int x = (offset / 4) % _screen.width;
-  int y = (offset / 4) / _screen.width;
-  _draw_rect(buf, x, y, len / 4, 1);
+  const uint32_t *pixels = buf;
+  size_t nr_pixels = len / sizeof(uint32_t);
+  size_t pos = offset / sizeof(uint32_t);
+
+  while (nr_pixels > 0) {
+    int x = pos % _screen.width;
+    int y = pos / _screen.width;
+    int n = _screen.width - x;
+
+    if ((size_t)n > nr_pixels) {
+      n = nr_pixels;
+    }
+
+    _draw_rect((uint32_t *)pixels, x, y, n, 1);
+    pixels += n;
+    pos += n;
+    nr_pixels -= n;
+  }
 }
 
 void init_device() {
