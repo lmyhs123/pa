7f6426366f394428ce95756139d5fb495068ed88
tracer-ics2017 <tracer@njuics.org>
2026-06-03 12:33:36 +0800

    >  run 2413982 lmy Linux lmy-VMware-Virtual-Platform 6.17.0-29-generic #29~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon May 11 10:30:58 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:33:36 up  7:07,  3 users,  load average: 0.14, 0.07, 0.24 d089e2907f29101f48dda5745354f0c57e737bd4

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -9,34 +9,15 @@ static const char *keyname[256] __attribute__((used)) = {
 };
 
 size_t events_read(void *buf, size_t len) {
-  static char event[32];
-  static size_t event_len = 0;
-  static size_t event_pos = 0;
-
-  if (event_pos == event_len) {
-    int key = _read_key();
-    int n;
-
-    if (key == _KEY_NONE) {
-      n = snprintf(event, sizeof(event), "t %lu\n", _uptime());
-    } else {
-      const char *type = (key & 0x8000) ? "kd" : "ku";
-      int keycode = key & ~0x8000;
-      n = snprintf(event, sizeof(event), "%s %s\n", type, keyname[keycode]);
-    }
-
-    event_len = n < sizeof(event) ? n : sizeof(event) - 1;
-    event_pos = 0;
+  int key = _read_key();
+  if (key == _KEY_NONE) {
+    return 0;
   }
 
-  size_t read_len = event_len - event_pos;
-  if (read_len > len) {
-    read_len = len;
-  }
-
-  memcpy(buf, event + event_pos, read_len);
-  event_pos += read_len;
-  return read_len;
+  const char *type = (key & 0x8000) ? "kd" : "ku";
+  int keycode = key & ~0x8000;
+  int n = snprintf(buf, len, "%s %s\n", type, keyname[keycode]);
+  return n < len ? n : len;
 }
 
 static char dispinfo[128] __attribute__((used));
@@ -46,24 +27,9 @@ void dispinfo_read(void *buf, off_t offset, size_t len) {
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
-  const uint32_t *pixels = buf;
-  size_t nr_pixels = len / sizeof(uint32_t);
-  size_t pos = offset / sizeof(uint32_t);
-
-  while (nr_pixels > 0) {
-    int x = pos % _screen.width;
-    int y = pos / _screen.width;
-    int n = _screen.width - x;
-
-    if ((size_t)n > nr_pixels) {
-      n = nr_pixels;
-    }
-
-    _draw_rect((uint32_t *)pixels, x, y, n, 1);
-    pixels += n;
-    pos += n;
-    nr_pixels -= n;
-  }
+  int x = (offset / 4) % _screen.width;
+  int y = (offset / 4) / _screen.width;
+  _draw_rect(buf, x, y, len / 4, 1);
 }
 
 void init_device() {
